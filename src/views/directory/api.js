34f35e1efe4a56566e41de352fa24970f3b0d3a4
hcl <625217314@qq.com>
2019-07-30 11:19:54 +0800

    文档更新

--- src/views/directory/api.js
+++ src/views/directory/api.js
@@ -83,8 +83,8 @@ export default [
       {
         '参数': 'fields',
         '说明': '以列表方式展示时, 需要展示的字段',
-        '类型': 'Object',
-        '默认值': ['name']
+        '类型': 'Array',
+        '默认值': [{ name: 'name' }]
       },
       {
         '参数': 'show-checkbox',
